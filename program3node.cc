f5c8f4e3d18b9cbe55e1448358d29dd17312f852
Sahil Patel <32462747+sahilpatel11@users.noreply.github.com>
2018-10-28 23:12:29 +0530

    Add files via upload

---
+++ program3node.cc
@@ -0,0 +1,93 @@
+// Include necessary header files
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/mobility-module.h"
+// Use ns3 namespace
+using namespace ns3;
+// Enable log for this program
+NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
+// Main function
+int
+main (int argc, char *argv[])
+{
+// Enable this program to read and parse command line arguments
+CommandLine cmd;
+cmd.Parse (argc, argv);
+// Enable Log of echo applications
+LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+// Create nodes
+NodeContainer nodes;
+nodes.Create (3);
+// Create a point-to-point channel and configure its attributes
+PointToPointHelper pointToPoint;
+pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+// Create a Network Device Container
+NetDeviceContainer device1,device2;
+device1 = pointToPoint.Install (nodes.Get(0),nodes.Get(1));
+device2 = pointToPoint.Install (nodes.Get(2),nodes.Get(1));
+// Install network stack on nodes
+InternetStackHelper stack;
+stack.Install (nodes);
+
+// Set network address and subnet mask
+Ipv4AddressHelper address;
+address.SetBase ("10.1.1.0", "255.255.255.0");
+// Assign IP address to every interface
+Ipv4InterfaceContainer interfaces1 = address.Assign (device1);
+Ipv4InterfaceContainer interfaces2 = address.Assign (device2);
+// Configure a Server application
+UdpEchoServerHelper echoServer (9);
+// Install Server application on a specific node
+ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+// Set start and stop time for Server application
+serverApps.Start (Seconds (1.0));
+serverApps.Stop (Seconds (10.0));
+// Configure a Client application
+UdpEchoClientHelper echoClient (interfaces1.GetAddress (1), 9);
+echoClient.SetAttribute ("MaxPackets", UintegerValue (5));
+echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+// Install Client application on a specific node
+ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+// Set start and stop time for Client application
+clientApps.Start (Seconds (2.0));
+clientApps.Stop (Seconds (10.0));
+
+
+UdpEchoClientHelper echoClient1 (interfaces2.GetAddress (1), 9);
+echoClient1.SetAttribute ("MaxPackets", UintegerValue (5));
+echoClient1.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+echoClient1.SetAttribute ("PacketSize", UintegerValue (1024));
+// Install Client application on a specific node
+ApplicationContainer clientApps1 = echoClient1.Install (nodes.Get (2));
+// Set start and stop time for Client application
+clientApps1.Start (Seconds (3.5));
+clientApps1.Stop (Seconds (10.0));
+
+pointToPoint.EnablePcapAll ("3node");
+
+MobilityHelper mobility;
+mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+mobility.Install (nodes);
+AnimationInterface anim ("3node.xml");
+AnimationInterface::SetConstantPosition (nodes.Get (0), 10, 25);
+AnimationInterface::SetConstantPosition (nodes.Get (1), 40, 25);
+AnimationInterface::SetConstantPosition (nodes.Get (2), 70, 25);
+anim.EnablePacketMetadata (true);
+
+AsciiTraceHelper ascii;
+pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("3node.tr"));
+
+// Run the simulation
+Simulator::Run ();
+// Release resources at the end of the simulation
+Simulator::Destroy ();
+return 0;
+}
